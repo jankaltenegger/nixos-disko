d39a4157d1fac0196cacd6d2a31572518b5193b3
jankaltenegger <jkaltenegger@ggc.edu>
2025-09-29 15:05:43 -0400

    Testing out new declarative managing of filesystem

---
+++ machine/ymir/disko.nix
@@ -0,0 +1,48 @@
+{
+  disko.devices = {
+    disk.main = {
+      device = "/dev/nvme0n1";
+      type = "disk";
+      content = {
+        type = "gpt";
+        partitions = {
+          ESP = {
+            name = "ESP";
+            size = "512M";
+            type = "EF00";
+            content = {
+              type = "filesystem";
+              format = "vfat";
+              mountpoint = "/boot/efi";
+              mountOptions = ["umask=0077"];
+            };
+          };
+          nix = {
+            size = "100%";
+            content = {
+              type = "luks";
+              name = "niflheim";
+              settings = {
+                allowDiscards = true;
+                keyFile = "/etc/secrets/initrd/keyfile.bin";
+              };
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/nix";
+              };
+            };
+          };
+        };
+      };
+    };
+    nodev."/" = {
+      fsType = "tmpfs";
+      mountOptions = [
+        "size=8G"
+        "defaults"
+        "mode=755"
+      ];
+    };
+  };
+}
