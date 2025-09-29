001dd075bec72033c2c492d927a8931a1ae616cb
jankaltenegger <jkaltenegger@ggc.edu>
2025-09-29 15:56:58 -0400

    Name change

--- flake.nix
+++ flake.nix
@@ -1,14 +1,9 @@
 {
   description = "My personal disko setups for different machines";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    disko.url = "github:nix-community/disko";
-  };
-
-  outputs = { self, nixpkgs, disko, ... }@inputs: {
+  outputs = {self, ...} @ inputs: {
     templates = {
-      host = {
+      ymir = {
         path = ./machine/ymir;
       };
     };
